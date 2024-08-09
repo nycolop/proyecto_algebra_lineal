8662672069e475d7efc7f64b0f445953ddc7835f
egecarli <gcarlaelisabeth@gmail.com>
2024-08-09 01:22:39 -0300

    cifrado sin inversa

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static encrypted.HillCipher.decrypt;
+import static encrypted.HillCipher.encrypt;
+
+import java.util.Scanner;
+
 public class Main {
-
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
 
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-    }
+    int[][] key = {
+        {6, 24, 1},
+        {13, 16, 10},
+        {20, 17, 15}
+    };
+
+
+    //tengo que calcular bien la inversa,esta no funciona, la saque re mal xD
+    int[][] keyDesencript = {{18,5,25},{21,18,10},{16,5,14}};
+
+    System.out.println(key.length);
+    String message = "Hola amiguitos como va";
+
+    String encryptedMessage = encrypt(message, key);
+    System.out.println("Texto cifrado: " + encryptedMessage);
+    String keyTodes = "ZJXMQYEKQYCROQGNSNDAI";
+
+    String descryp = decrypt(keyTodes,keyDesencript);
+    System.out.println("Texto descifrado: "+ descryp);
+
   }
+
 }
