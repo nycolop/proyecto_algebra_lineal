04fc5d656aab0aeb646759dfec4fa4cd666b9bff
Luciano Moisé <luciano.moise@jala.university>
2024-08-09 14:11:17 -0300

    Se agrego metodo de desencriptado

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-import static encrypted.MatrizEncripted.encriptacion;
 
-import encrypted.MatrizEncripted;
-import java.util.Scanner;
+import static encrypted.MatrizEncripted.desencriptar;
+import static encrypted.MatrizEncripted.encriptacion;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    String inputMessage = "holaeduardo";
-
+    String inputMessage = "pileta";
+    System.out.println("El mensaje es:" + inputMessage);
 
     String mensajeEncriptado = encriptacion(inputMessage);
     System.out.println("Mensaje encriptado: " + mensajeEncriptado);
+
+    String mensajeDesencriptado = desencriptar(mensajeEncriptado);
+    System.out.println("Mensaje desencriptado: " + mensajeDesencriptado);
+
 
 
   }
