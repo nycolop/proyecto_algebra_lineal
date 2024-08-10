13c3187f7946dd2bfbef4e1c82c8dbce24519a6e
nycolop <36177527+nycolop@users.noreply.github.com>
2024-08-10 18:43:12 -0300

    Añade una interfaz grafica al proyecto

---
+++ src/Interfaz.java
@@ -0,0 +1,91 @@
+
+import static encrypted.MatrizEncripted.desencriptar;
+import static encrypted.MatrizEncripted.encriptacion;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Interfaz extends JFrame {
+
+  public Interfaz() {
+    setTitle("Proyecto algebra linear - Encriptar/Desencriptar");
+    setSize(520, 600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel panel = new JPanel();
+    add(panel);
+    colocarComponentes(panel);
+  }
+
+  private void colocarComponentes(JPanel panel) {
+    panel.setLayout(null);
+
+    JLabel titulo = new JLabel("Proyecto Algebra lineal");
+    JLabel subtitulo = new JLabel("Cifrado Hill con matrices");
+    JTextArea creditos = new JTextArea("Hecho por: \nNicolas \nLuciano \nCarla \nMicaela");
+    titulo.setBounds(100, 20, 300, 100);
+    subtitulo.setBounds(100, 40, 300, 100);
+    creditos.setBounds(100, 400, 200, 200);
+
+    creditos.setBackground(new Color(238, 238, 238));
+    titulo.setFont(new Font("Arial", Font.BOLD, 24));
+    subtitulo.setFont(new Font("Arial", Font.BOLD, 24));
+    creditos.setEditable(false);
+
+    panel.add(titulo);
+    panel.add(subtitulo);
+    panel.add(creditos);
+
+
+
+    JLabel label = new JLabel("Ingrese un texto:");
+    label.setBounds(100, 75 + 50, 300, 25);
+    panel.add(label);
+
+    JTextField textoInput = new JTextField();
+    textoInput.setBounds(100, 100 + 50, 300, 25);
+    panel.add(textoInput);
+
+    JLabel textoOutputPrefix = new JLabel("Salida: ");
+    textoOutputPrefix.setBounds(100 , 100 + 50, 100, 100);
+    panel.add(textoOutputPrefix);
+
+    JLabel textoOutput = new JLabel("");
+    textoOutput.setBounds(150, 100 + 50, 100, 100);
+    panel.add(textoOutput);
+
+    JButton botonEncriptar = new JButton("Encriptar");
+    botonEncriptar.setBounds(100, 220 + 50, 300, 25);
+    panel.add(botonEncriptar);
+
+    JButton botonDesencriptar = new JButton("Desencriptar");
+    botonDesencriptar.setBounds(100, 260 + 50, 300, 25);
+    panel.add(botonDesencriptar);
+
+    botonEncriptar.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        String inputMessage = textoInput.getText();
+        String mensajeEncriptado = encriptacion(inputMessage);
+        textoOutput.setText(mensajeEncriptado);
+      }
+    });
+
+    botonDesencriptar.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        String inputMessage = textoInput.getText();
+        String mensajeDesencriptado = desencriptar(inputMessage);
+        textoOutput.setText(mensajeDesencriptado);
+      }
+    });
+  }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,10 @@
 
-import static encrypted.MatrizEncripted.desencriptar;
-import static encrypted.MatrizEncripted.encriptacion;
+
 
 public class Main {
 
   public static void main(String[] args) {
-
-    String inputMessage = "pileta";
-    System.out.println("El mensaje es:" + inputMessage);
-
-    String mensajeEncriptado = encriptacion(inputMessage);
-    System.out.println("Mensaje encriptado: " + mensajeEncriptado);
-
-    String mensajeDesencriptado = desencriptar(mensajeEncriptado);
-    System.out.println("Mensaje desencriptado: " + mensajeDesencriptado);
-
-
-
+    Interfaz ventana = new Interfaz();
+    ventana.setVisible(true);
   }
 };
